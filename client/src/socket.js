8a1bd17e70f1d9a00631204cc7988a46a1202a1a
Joao Francisco <joao.netinhu43@gmail.com>
2026-01-24 19:39:45 -0300

    Configurando deploy para produção

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 // Detecta automaticamente se está em dev ou prod
 const URL = import.meta.env.PROD 
-  ? 'https://entreamigos-game.onrender.com' // Sua URL de prod
+  ? 'https://www.entreamigos.app.br' // Sua URL de prod
   : (import.meta.env.VITE_API_URL || 'http://localhost:3001');
 
 export const socket = io(URL, {
