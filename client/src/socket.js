8739d6eeb28295023eeeb5cb27579798fbc8adfd
Joao Francisco <joao.netinhu43@gmail.com>
2026-01-26 21:42:30 -0300

    tentativa de correcao de conexao com o servidor

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,12 +1,10 @@
-// client/src/socket.js
 import { io } from 'socket.io-client';
 
-// Se estiver em produção (site online), usa a raiz '/' (o Nginx cuida do resto)
-// Se estiver no seu PC desenvolvendo, usa localhost:3001
-const URL = import.meta.env.PROD ? undefined : 'http://localhost:3001';
+// Força o endereço HTTPS correto (Versão Blindada)
+const URL = 'https://entreamigos.app.br';
 
 export const socket = io(URL, {
-    transports: ['websocket', 'polling'], // Força métodos compatíveis
+    transports: ['websocket', 'polling'],
     autoConnect: true,
     reconnection: true,
 });
