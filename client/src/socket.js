8f8aaad08ac69359001a938e503fd8269e5eba36
Joao Francisco <joao.netinhu43@gmail.com>
2026-01-26 21:54:16 -0300

    Fix: Hardcoded socket URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,11 @@
 import { io } from 'socket.io-client';
 
-// Força o endereço HTTPS correto (Versão Blindada)
+// Forçamos o endereço seguro do seu site
 const URL = 'https://entreamigos.app.br';
 
 export const socket = io(URL, {
-    transports: ['websocket', 'polling'],
+    path: '/socket.io/',
+    transports: ['polling', 'websocket'], // Tenta HTTP primeiro, depois WS
     autoConnect: true,
     reconnection: true,
 });
